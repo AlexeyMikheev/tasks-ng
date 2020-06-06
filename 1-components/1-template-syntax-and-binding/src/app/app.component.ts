1b78ee7e3534beafcbdae2256be1d76ab1fcf036
dagas_88@mail.ru <dagas_88@mail.ru>
2020-06-06 18:49:27 +0300

    Task 1

--- 1-components/1-template-syntax-and-binding/src/app/app.component.ts
+++ 1-components/1-template-syntax-and-binding/src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { IProduct, product } from '../mocks/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public product: IProduct = product;
+
+  public chosenProduct!: IProduct;
+
+  addProduct($event: IProduct) {
+    this.chosenProduct = $event;
+  }
+}
